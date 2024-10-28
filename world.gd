75e9ad6bba00c6d3c5d4f442d914f2d200202dc3
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-10-28 12:50:50 -0400

    add 2 cars

--- world.gd
+++ world.gd
@@ -6,9 +6,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	const move_speed := 0.5
+	const move_speed_B1 := 0.4
+	const move_speed_B2 := 0.30
+	const move_speed_B3 := 0.20
 	print(delta)
-	%PathFollow3D_Car2.progress += move_speed + delta
-	%PathFollow3D.progress = move_speed * delta
+	%PathFollow3D_CarB1.progress += move_speed_B1 + delta
+	%PathFollow3D_CarB2.progress += move_speed_B2 + delta
+	%PathFollow3D_CarB3.progress += move_speed_B3 + delta
+
+
 
 	
